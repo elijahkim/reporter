72071d2869f0859a0cf5c6f1264dafde6d2ffc1e
Eli <kimelijah90@gmail.com>
2020-08-12 18:17:55 -0400

    Add polyfills

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,25 @@
 // We need to import the CSS so that webpack will load it.
 // The MiniCssExtractPlugin is used to separate it out into
 // its own CSS file.
-import "../css/app.scss"
+import "mdn-polyfills/Object.assign";
+import "mdn-polyfills/CustomEvent";
+import "mdn-polyfills/String.prototype.startsWith";
+import "mdn-polyfills/Array.from";
+import "mdn-polyfills/Array.prototype.find";
+import "mdn-polyfills/Array.prototype.some";
+import "mdn-polyfills/NodeList.prototype.forEach";
+import "mdn-polyfills/Element.prototype.closest";
+import "mdn-polyfills/Element.prototype.matches";
+import "mdn-polyfills/Node.prototype.remove";
+import "child-replace-with-polyfill";
+import "url-search-params-polyfill";
+import "formdata-polyfill";
+import "classlist-polyfill";
+import "@webcomponents/template";
+import "shim-keyboard-event-key";
+import "core-js/features/set";
+
+import "../css/app.scss";
 
 // webpack automatically bundles all modules in your
 // entry points. Those entry points can be configured
@@ -12,24 +30,27 @@ import "../css/app.scss"
 //     import {Socket} from "phoenix"
 //     import socket from "./socket"
 //
-import "phoenix_html"
-import {Socket} from "phoenix"
-import NProgress from "nprogress"
-import {LiveSocket} from "phoenix_live_view"
+import "phoenix_html";
+import { Socket } from "phoenix";
+import NProgress from "nprogress";
+import { LiveSocket } from "phoenix_live_view";
 
-let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
-let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}})
+let csrfToken = document
+  .querySelector("meta[name='csrf-token']")
+  .getAttribute("content");
+let liveSocket = new LiveSocket("/live", Socket, {
+  params: { _csrf_token: csrfToken },
+});
 
 // Show progress bar on live navigation and form submits
-window.addEventListener("phx:page-loading-start", info => NProgress.start())
-window.addEventListener("phx:page-loading-stop", info => NProgress.done())
+window.addEventListener("phx:page-loading-start", (info) => NProgress.start());
+window.addEventListener("phx:page-loading-stop", (info) => NProgress.done());
 
 // connect if there are any LiveViews on the page
-liveSocket.connect()
+liveSocket.connect();
 
 // expose liveSocket on window for web console debug logs and latency simulation:
 // >> liveSocket.enableDebug()
 // >> liveSocket.enableLatencySim(1000)  // enabled for duration of browser session
 // >> liveSocket.disableLatencySim()
-window.liveSocket = liveSocket
-
+window.liveSocket = liveSocket;
